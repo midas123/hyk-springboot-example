c70f8406dba3cdd7ac4180eb29e2d5ef20007785
midas <midas@host.docker.internal>
2020-10-16 10:10:04 +0900

    initial

---
+++ src/main/java/com/example/demo/HykSpringbootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HykSpringbootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HykSpringbootExampleApplication.class, args);
+	}
+
+}
